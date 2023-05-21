f68db2bc3ec867ca657cea1cf18437f0175cb57d
Janos Jung <Janos.Jung@outlook.com>
2023-05-21 23:49:01 +0200

    added Manager class

---
+++ ManagedRunspacePool/RunspaceManager.cs
@@ -0,0 +1,49 @@
+﻿using ManagedRunspacePool2;
+using System;
+
+namespace ManagedRunspacePool
+{
+    public class RunspaceManager : IDisposable
+    {
+        private bool disposedValue;
+
+
+        public void CreateAgent(string name, RunspaceAgentSettings settings)
+        {
+
+        }
+
+        public void GetAgent(string name)
+        {
+
+        }
+
+        public void ShutDown()
+        {
+
+        }
+
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects)
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
